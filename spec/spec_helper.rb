b985e1f9418c3b152968e4a8fdbc42f4c85918e4
Naila Nasleem <NNasleem@spartaglobal.com>
2019-08-14 14:56:54 +0100

    classwork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 require 'chefspec'
 require 'chefspec/policyfile'
+
+at_exit {ChefSpec::Coverage.report!} # report for testing (any recipes without unit testing, reports to the developer to write unit test )
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,5 +1,3 @@
-# InSpec test for recipe node::default
-
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
@@ -11,6 +9,28 @@ unless os.windows?
 end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+# describe port(80), :skip do
+#   it { should_not be_listening }
+# end
+
+describe port(80) do
+  it { should be_listening }
 end
+
+#### TEST PROMISE #########
+describe service ('nginx') do
+ it {should be_enabled }
+ it { should be_running }
+end
+
+
+# # here you are spinning a thread, so when one processes stop,
+# others are still working and then you can get the status at the end. cmp # compare
+describe http('http://localhost', enabled_remote_worker: true) do
+  its('status'){ should cmp 502 }
+end
+# do a test to start
+
+# do a tets to enable
+
+# then do kitchen test
